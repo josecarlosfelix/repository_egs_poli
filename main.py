d30d22599451359531ea32c005b1a8723329ec5e
Adjailson F. Melo <afm4@ecomp.poli.br>
2024-06-20 19:42:01 -0300

    Outros testes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html",dados="Olá Mundo!")
+    return render_template("index.html",dados="Olá Mundo! (Exemplo de enviar dados para a página)")
 
 
 if __name__ in "__main__":
